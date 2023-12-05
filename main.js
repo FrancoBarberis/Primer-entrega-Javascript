143f6354443a82f5edae39bf26708e269b0b7b1d
FrancoBarberis <francobarberissic3@gmail.com>
2023-12-04 23:52:43 -0300

    estructura base

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let productoElegido = prompt("Ingrese el codigo de producto deseado");
+//ESTO ES UN SIMULADOR DE GOLOSINAS EN EL QUE SE INGRESA UN NUMERO PARA RECIBIR CIERTO PRODUCTO
+//
+
+let productoElegido = prompt("Ingrese el codigo de producto deseado (1, 2 o 3)");
+let chupetin = 0;
+let chocolate = 0;
+let alfajor = 0;
+for (let index = 0; index < 3; index++) {
+    if (productoElegido == 1) {
+        chupetin
+    }
+
+}
