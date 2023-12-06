4444283029c084ba033b0910f7cbf2dfc05a5bea
FrancoBarberis <francobarberissic3@gmail.com>
2023-12-06 14:15:44 -0300

    Corte workflow automatico por presupuesto

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
 
 
-let presupuesto = 100;
+let presupuesto = 30;
 let cantChocolates = 0;
 let cantChupetines = 0;
 let cantAlfajores = 0;
 let sigueComprando = true;
+
+
+function elegir() {
+    let eleccion = parseInt(prompt("PRESUPUESTO: $" + presupuesto + ", (OPCION 1)Precio Chocolates: $30" + ", (OPCION 2)PrecioAlfajores: $20" + ", (OPCION 3)Precio Chupetines: $10"));
+    return eleccion;
+}
+function preguntarSiSigue() {
+    let sigue = prompt("¿Desea seguir comprando? (si/no)");
+    return sigue;
+}
+function alertarValidez() {
+    alert("Ingrese un valor válido (si o no)");
+}
 while (presupuesto > 0 && sigueComprando) {
-    let eleccion = parseInt(prompt("Presupuesto: " + presupuesto + ", (OPCION 1)Precio Chocolates: 30" + ", (OPCION 2)PrecioAlfajores: 20" + ", (OPCION 3)Precio Chupetines: 10"));
+    let eleccion = elegir();
     switch (eleccion) {
         case 1:
             if (presupuesto >= 30) {
                 cantChocolates++;
                 presupuesto -= 30;
-                let bandera = true;
-                while (bandera) {
-                    let sigue = prompt("Desea seguir comprando? (si o no)" + " ,Presupuesto: " + presupuesto);
-                    switch (sigue) {
-                        case "si":
-                            sigueComprando = true;
-                            bandera = false;
-                            break;
-                        case "no":
-                            sigueComprando = false;
-                            bandera = false;
-                            break;
-                        default:
-                            alert("Ingrese un valor válido (si o no)" + " ,Presupuesto: " + presupuesto);
-                            break;
+                if (presupuesto > 0) {
+                    let bandera = true;
+                    while (bandera) {
+                        let sigue = preguntarSiSigue();
+                        switch (sigue) {
+                            case "si":
+                                sigueComprando = true;
+                                bandera = false;
+                                break;
+                            case "no":
+                                sigueComprando = false;
+                                bandera = false;
+                                break;
+                            default:
+                                alertarValidez();
+                                break;
+                        }
                     }
                 }
+
                 break;
             } else {
                 alert("No alcanza el presupuesto para comprar chocolates");
@@ -38,21 +54,23 @@ while (presupuesto > 0 && sigueComprando) {
             if (presupuesto >= 20) {
                 cantAlfajores++;
                 presupuesto -= 20;
-                let bandera = true;
-                while (bandera) {
-                    let sigue = prompt("Desea seguir comprando? (si o no)" + " ,Presupuesto: " + presupuesto);
-                    switch (sigue) {
-                        case "si":
-                            sigueComprando = true;
-                            bandera = false;
-                            break;
-                        case "no":
-                            sigueComprando = false;
-                            bandera = false;
-                            break;
-                        default:
-                            alert("Ingrese un valor válido (si o no)" + " ,Presupuesto: " + presupuesto);
-                            break;
+                if (presupuesto > 0) {
+                    let bandera = true;
+                    while (bandera) {
+                        let sigue = preguntarSiSigue();
+                        switch (sigue) {
+                            case "si":
+                                sigueComprando = true;
+                                bandera = false;
+                                break;
+                            case "no":
+                                sigueComprando = false;
+                                bandera = false;
+                                break;
+                            default:
+                                alertarValidez();
+                                break;
+                        }
                     }
                 }
                 break;
@@ -64,26 +82,27 @@ while (presupuesto > 0 && sigueComprando) {
         case 3:
             if (presupuesto >= 10) {
                 cantChupetines++;
-                presupuesto -= 10; let bandera = true;
-                while (bandera) {
-                    let sigue = prompt("Desea seguir comprando? (si o no)" + " ,Presupuesto: " + presupuesto);
-                    switch (sigue) {
-                        case "si":
-                            sigueComprando = true;
-                            bandera = false;
-                            break;
-                        case "no":
-                            sigueComprando = false;
-                            bandera = false;
-                            break;
-                        default:
-                            alert("Ingrese un valor válido (si o no)" + " ,Presupuesto: " + presupuesto);
-                            break;
+                presupuesto -= 10;
+                if (presupuesto > 0) {
+                    let bandera = true;
+                    while (bandera) {
+                        let sigue = preguntarSiSigue();
+                        switch (sigue) {
+                            case "si":
+                                sigueComprando = true;
+                                bandera = false;
+                                break;
+                            case "no":
+                                sigueComprando = false;
+                                bandera = false;
+                                break;
+                            default:
+                                alertarValidez();
+                                break;
+                        }
                     }
                 }
                 break;
-
-
             } else {
                 alert("No alcanza el presupuesto para comprar chupetines");
                 break;
@@ -92,4 +111,5 @@ while (presupuesto > 0 && sigueComprando) {
             alert("Ingrese un valor válido (1/2/3)");
             break;
     }
-    alert("Usted recibe: " + "Chocolates: " + cantChocolates + " Chupetines: " + cantChupetines + " Alfajores: " + cantAlfajores);
+}
+alert("USTED RECIBE: " + "Chocolates: " + cantChocolates + " ,Chupetines: " + cantChupetines + " ,Alfajores: " + cantAlfajores + " PRESUPUESTO RESTANTE: $" + presupuesto);
